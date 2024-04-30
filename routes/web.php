1d76f015184c5896bab2c8b4ec7eaee3fa6d0053
danieln85 <danieln85@gmail.com>
2024-04-30 14:38:35 -0500

    vistas crear recibos y recibos de caja

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CashReceiptController;
+
+
+Route::resource('cash-receipt', CashReceiptController::class);
+Route::get('recibos-de-caja', [CashReceiptController::class, 'index'])->name('cash-receipts');
+Route::get('crear-recibos-de-caja', [CashReceiptController::class, 'create'])->name('create-cash-receipts');
 
 Route::get('/', function () {
     return view('welcome');
