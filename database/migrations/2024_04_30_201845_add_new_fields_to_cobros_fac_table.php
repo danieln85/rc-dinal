bfc00e644656dfe598d67aaee401ffa181154bc4
danieln85 <danieln85@gmail.com>
2024-04-30 17:11:42 -0500

    columna status

--- database/migrations/2024_04_30_201845_add_new_fields_to_cobros_fac_table.php
+++ database/migrations/2024_04_30_201845_add_new_fields_to_cobros_fac_table.php
@@ -12,10 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('cobros_fac', function (Blueprint $table) {
-            $table->string('nombre_cliente')->nullable();
-            $table->string('recibido_admin')->nullable();
-            $table->string('recibido_supervisor')->nullable();
-            $table->timestamps();
+            // $table->string('nombre_cliente')->nullable();
+            // $table->string('recibido_admin')->nullable();
+            // $table->string('recibido_supervisor')->nullable();
+            $table->string('status')->nullable();
+            // $table->timestamps();
+
+            // $table->date('date_cobro')->nullable();
+            // $table->string('num_factura')->nullable();
+            // $table->string('cobro_abono')->nullable();
+            // $table->string('abono')->nullable();
+            // $table->string('descuento')->nullable();
+            // $table->string('retencion')->nullable();
+            // $table->string('devolucion')->nullable();
+            // $table->string('metodo_pago')->nullable();
         });
     }
 
@@ -25,7 +35,18 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('cobros_fac', function (Blueprint $table) {
-            $table->dropColumn(['recibido_admin', 'recibido_supervisor']);
+            // $table->string('nombre_cliente')->nullable();
+            // $table->string('recibido_admin')->nullable();
+            // $table->string('recibido_supervisor')->nullable();
+            $table->string('status')->nullable();
+            // $table->dropColumn('date_cobro');
+            // $table->dropColumn('num_factura');
+            // $table->dropColumn('cobro_abono');
+            // $table->dropColumn('abono');
+            // $table->dropColumn('descuento');
+            // $table->dropColumn('retencion');
+            // $table->dropColumn('devolucion');
+            // $table->dropColumn('metodo_pago');
             $table->dropTimestamps();
         });
     }
