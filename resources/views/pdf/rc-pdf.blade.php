73ea1ab5a7351a5328ee2df36d1434dfba28091a
danieln85 <danieln85@gmail.com>
2024-05-09 11:40:32 -0500

    recibo de caja para descargar, terminado

--- resources/views/pdf/rc-pdf.blade.php
+++ resources/views/pdf/rc-pdf.blade.php
@@ -1,121 +1,169 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title>Recibo de Caja</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
-            margin: 40px;
-            font-size: 14px;
+            margin: 20px;
+            font-size: 16px;
+            color: #333;
+            background-color: #f7f7f7;
         }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            border-radius: 10px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
         .header {
             text-align: center;
             margin-bottom: 20px;
         }
+
         .logo {
             width: 200px;
-			height: 70px;
+            height: auto;
         }
-        .content {
-            margin-top: 20px;
+
+        h1 {
+            font-size: 24px;
+            margin: 10px 0;
         }
+
+        h2 {
+            font-size: 20px;
+            margin: 5px 0;
+            color: #007bff;
+        }
+
         table {
             width: 100%;
             border-collapse: collapse;
+            margin-top: 20px;
         }
-        th, td {
+
+        th,
+        td {
             border: 1px solid #ddd;
-            padding: 8px;
+            padding: 10px;
             text-align: left;
         }
+
         th {
             background-color: #f2f2f2;
         }
+
         .footer {
             text-align: center;
             margin-top: 30px;
+            font-size: 14px;
+            color: #777;
+        }
+
+        .note {
             font-size: 12px;
+            color: #777;
+            margin-top: 10px;
         }
-		h2 {
+
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        .mb-2{
+            margin-bottom: 5px;
+        }
+
+        .text-start{
             text-align: left;
-            margin-top: 0; 
-            
         }
-		
     </style>
 </head>
+
 <body>
-    <div class="header">
-        <img src="https://rc.dinalpartes.com/images/logo/logo-dinalpartes.png" alt="Logo-Dinalpartes" class="logo">
-        <h1>Recibo de Caja</h1>
-		<h2>No. {{ $datos->numero_rc }}</h2>
-    </div>
-    <div class="content">
+    <div class="container">
+        <div class="header">
+            <img src="https://rc.dinalpartes.com/images/logo/logo-dinalpartes.png" alt="Logo-Dinalpartes" class="logo">
+            <h1>Recibo de Caja</h1>
+            <h2 class="text-start">No. {{ $datos->numero_rc }}</h2>
+        </div>
         <table>
-			<tr>
+            <tr>
                 <th>Fecha</th>
                 <td>{{ $datos->date_cobro }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>NIT</th>
                 <td>{{ $datos->nit }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Cliente</th>
                 <td>{{ $datos->nombre_cliente }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>No Factura</th>
                 <td>{{ $datos->num_factura }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Valor Recibido</th>
                 <td>${{ $datos->cobro_abono }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Método de pago</th>
                 <td>{{ ucfirst(trim($datos->metodo_pago, ',')) }}</td>
             </tr>
             <tr>
                 <th>Abono</th>
-                <td>@if($datos->abono == '')
-					N/A
-					@else
-					{{ $datos->abono }}
-					@endif
-</td>
+                <td>@if($datos->abono == '') N/A @else {{ $datos->abono }} @endif</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Retención</th>
                 <td>${{ $datos->retencion ? $datos->retencion : '0.00' }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Devolución</th>
                 <td>${{ $datos->devolucion ? $datos->devolucion : '0.00' }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Descuentos</th>
                 <td>${{ $datos->descuento ? $datos->descuento : '0.00' }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Observaciones</th>
                 <td>{{ $datos->informacion }}</td>
             </tr>
-			<tr>
+            <tr>
                 <th>Cobrado por</th>
                 <td>{{ $datos->cobrado_por }}</td>
             </tr>
         </table>
-    </div>
-    <div class="footer">
-				<p><ul>
-					<li>Exige siempre tu recibo de caja por cada pago que realices en efectivo por tus facturas a crédito.</li>
-					<li>Este recibo de caja es el único comprobante valido de tus pagos en Efectivo y/o por Nequi.</li>
-					</ul>
-				</p>
-		<h4>Este recibo de caja es generado una vez realices el pago en efectivo de una factura de venta a crédito.</h4>
-        <p>Gracias por preferirnos, visítanos en <a href="https://www.dinalpartes.com">www.dinalpartes.com</a></p>
+        <div class="footer">
+            <p class="note">Nota: Este recibo de caja es generado una vez realices el pago en efectivo de una factura de venta a crédito.
+            </p>
+            <p>
+                <h4 class="text-start">Recomendaciones:</h4>
+                <ul>
+                    <li class="mb-2">Exige tu recibo de caja por los pagos que realices en efectivo de tus facturas a crédito.</li>
+
+                <li>Este recibo de caja es el único comprobante válido por pagos en efectivo.</li>
+            </p>
+
+            <p>Gracias por preferirnos, visítanos en <a href="https://www.dinalpartes.com">www.dinalpartes.com</a>
+            </p>
+        </div>
     </div>
 </body>
+
 </html>
